181989e924db6cd31f0bda68de12c3b1b525ebfa
elsakti <svkakazone@gmail.com>
2024-07-03 00:53:48 +0700

    first-commit

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+function App() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+
+  function handlePrev() {
+    if (step > 1) setStep((step) => step - 1);
+  }
+
+  function handleNext() {
+    if (step < 3) setStep((step) => step + 1);
+  }
+
+  return (
+    <>
+      <button className="close" onClick={() => setIsOpen((isOpen) => !isOpen)}>
+        &times;
+      </button>
+      {isOpen &&
+        <div className="steps">
+        <div className="numbers">
+          <div className={step >= 1 ? "active" : ""}>1</div>
+          <div className={step >= 2 ? "active" : ""}>2</div>
+          <div className={step >= 3 ? "active" : ""}>3</div>
+        </div>
+        <p className="message">Hello World</p>
+        <div className="buttons">
+          <button
+            className="previous"
+            style={{ backgroundColor: "#526D82", color: "#fff" }}
+            onClick={handlePrev}
+          >
+            Prev
+          </button>
+          <button
+            className="next"
+            style={{ backgroundColor: "#526D82", color: "#fff" }}
+            onClick={handleNext}
+          >
+            Next
+          </button>
+        </div>
+      </div>
+      }
+    </>
+  );
+}
+
+export default App;
